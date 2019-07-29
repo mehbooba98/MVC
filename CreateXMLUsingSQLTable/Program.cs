573e07059254a29126332bb81a82bb07f0974a58
mehbooba98 <mehboob_a98@yahoo.com>
2019-07-28 23:29:44 -0400

    Add files via upload

---
+++ CreateXMLUsingSQLTable/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+
+namespace CreateXMLUsingSQLTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Connection string
+            SqlConnection cnAW = new SqlConnection("Data Source=LAPTOP-E1J6KUU3\\TECHBROTHERSSQL; user id=XMLUser; password=Pa$$w0rd2020; Initial Catalog=AdventureWorks2014;");
+
+
+            //Create Data Adapter
+            SqlDataAdapter daSalesReason = new SqlDataAdapter("SELECT * FROM [Sales].[SalesReason]", cnAW);
+
+            //Create data Set
+
+            DataSet ds = new DataSet();
+            cnAW.Open();
+
+            daSalesReason.Fill(ds, "SalesReason");
+            DataTable dt;
+                       
+
+            dt = ds.Tables["SalesReason"];
+            //This node is not adding
+            //dt.Columns.Add("Age", typeof(string));
+
+            //Read all records from SalesReason table
+            foreach (DataColumn dc in dt.Columns)
+            {
+                /*
+                if (dc.ColumnName == "Name")
+                {
+
+                    dc.ColumnMapping = MappingType.Element;
+                    dc.ExtendedProperties.Add("Age", 50);
+                    dc.ColumnMapping = MappingType.Attribute;
+
+                }
+                else
+                {*/
+                    dc.ColumnMapping = MappingType.Element;
+                //}
+            }
+
+            ds.WriteXml(@"C:\\TechnicalExp\\websites\\MVC-Sites\\SalesReason.xml");
+            Console.Write("Completed writing XML file, using a DataSet");
+            Console.Read();
+
+
+            dt.Clear();
+            cnAW.Close();
+
+
+        }
+    }
+}
---
+++ CreateXMLfile/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CreateXMLfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string xmlFile = "C:\\TechnicalExp\\websites\\MVC-Sites\\Employee.xml";
+            XmlTextWriter xmlWriter = new XmlTextWriter(xmlFile, Encoding.UTF8);
+
+            xmlWriter.Formatting = Formatting.Indented;
+
+
+            //Create Header node for XML
+            xmlWriter.WriteStartDocument();
+
+            xmlWriter.WriteComment("Creating an XML file using C#");
+
+            //Write Starting node for Employee records i.e Employees
+            xmlWriter.WriteStartElement("Employees");
+
+            //Create loop to accept input from user through consol
+
+            for (int i = 1; i <= 2; i++) {
+
+                //First Node 
+                xmlWriter.WriteStartElement("Employee");
+
+                //Employee Node has three Attributes
+                Console.WriteLine("Enter Id for Employee " + i);
+                xmlWriter.WriteElementString("ID", Console.ReadLine());
+
+                Console.WriteLine("Enter FullName for Employee " + i);
+                xmlWriter.WriteElementString("FullName", Console.ReadLine());
+
+                Console.WriteLine("Enter Department for Employee " + i);
+                xmlWriter.WriteElementString("Department", Console.ReadLine());
+
+                xmlWriter.WriteEndElement();
+
+            }
+
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Flush();
+            xmlWriter.Close();
+            
+        }
+    }
+}
